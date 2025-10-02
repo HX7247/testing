5c0a1c3611c4a4df17b8f427a173ae8070243c3b
HX7247 <HX7247>
2025-10-02 11:18:34 +0100

    commit4

--- unit_test.py
+++ unit_test.py
@@ -1,1 +1,12 @@
+import unittest
+from test_function  import addition
 
+class TestMyFunction(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(addition(2, 3), 5)
+
+    def test_case_2(self):
+        self.assertEqual(addition(-1, 1), 0)
+
+    def test_case_3(self):
+        self.assertEqual(addition(0, 0), 0)
