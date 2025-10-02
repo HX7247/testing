51ba24d809018c47568151e3ef5d1659c6ea6faa
HX7247 <HX7247>
2025-10-02 15:15:19 +0100

    commit15

--- line_unit_test.py
+++ line_unit_test.py
@@ -8,7 +8,8 @@ class TestLineFittingPipeline(unittest.TestCase):
         generate_synthetic_data(m=m_true, b=b_true, filename=filename)
         _, _, m_fit, b_fit = fit_line_from_csv(filename=filename)
         self.assertAlmostEqual(m_fit, m_true, delta=0.2)
-        self.assertAlmostEqual(b_fit, b_true, delta=0.5)
+        self.assertAlmostEqual(b_fit, b_true, delta=0.05)
+        print(m_fit , b_fit)
 
 if __name__ == "__main__":
     unittest.main()
